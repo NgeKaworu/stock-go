65cfbabc7ea86804bf22abca7058629011df01ef
NgeKaworu <NgeKaworu@163.com>
2020-03-24 23:11:50 +0800

    当前股价信息

--- src/controllers/CurrentInfoControoler.go
+++ src/controllers/CurrentInfoControoler.go
@@ -15,8 +15,11 @@ func (d *DbEngine) FetchCurrentInfo() {
 		panic(err)
 
 	}
-	defer resp.Body.Close()
 	s, err := ioutil.ReadAll(resp.Body)
-	log.Println(strings.Split(string(s), ","))
+	defer resp.Body.Close()
 	// 股票名称、今日开盘价、昨日收盘价、当前价格、今日最高价、今日最低价、竞买价、竞卖价、成交股数、成交金额、买1手、买1报价、买2手、买2报价、…、买5报价、…、卖5报价、日期、时间
+	strArr := strings.Split(string(s), ",")
+	for _, v := range strArr[1 : len(strArr)-2] {
+		log.Println(string(v))
+	}
 }
