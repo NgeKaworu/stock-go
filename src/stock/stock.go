14bee7d79e4d99ac8411b563edece4ab61aac586
NgeKaworu <NgeKaworu@163.com>
2020-04-02 23:38:30 +0800

    测试 bson -

--- src/stock/stock.go
+++ src/stock/stock.go
@@ -8,20 +8,20 @@ import (
 
 // Stock 股票基本结构
 type Stock struct {
-	Code        string               //股票代码
-	Bourse      string               //交易所名字
-	BourseCode  string               //交易所代码
-	Enterprise  *[]models.Enterprise //年报列表
-	CurrentInfo *models.CurrentInfo  //当前信息
-	Classify    string               //板块
-	PB          float64              //市净率
-	PE          float64              //市盈率
-	PEG         float64              //市盈增长比
-	ROE         float64              //净资产收益率
-	DPE         float64              //动态利润估值
-	DCE         float64              //动态现金估值
-	AAGR        float64              //平均年增长率
-	Grade       float64              //评分
+	Code        string               `bson:"code"`        //股票代码
+	Bourse      string               `bson:"bourse"`      //交易所名字
+	BourseCode  string               `bson:"bourse_code"` //交易所代码
+	Enterprise  *[]models.Enterprise `bson:"-"`           //年报列表
+	CurrentInfo *models.CurrentInfo  `bson:"-"`           //当前信息
+	Classify    string               `bson:"classify"`    //板块
+	PB          float64              `bson:"pb"`          //市净率
+	PE          float64              `bson:"pe"`          //市盈率
+	PEG         float64              `bson:"peg"`         //市盈增长比
+	ROE         float64              `bson:"roe"`         //净资产收益率
+	DPE         float64              `bson:"dpe"`         //动态利润估值
+	DCE         float64              `bson:"dce"`         //动态现金估值
+	AAGR        float64              `bson:"aagr"`        //平均年增长率
+	Grade       float64              `bson:"grade"`       //评分
 }
 
 // CusSort 自定义 排序
