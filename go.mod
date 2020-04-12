27dc116aa598f967a4871137635948055a5b740a
NgeKaworu <NgeKaworu@163.com>
2020-04-12 16:01:46 +0800

    加了名称

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module stock
 
 go 1.14
 
-require go.mongodb.org/mongo-driver v1.3.1
+require (
+	go.mongodb.org/mongo-driver v1.3.1
+	golang.org/x/text v0.3.2
+)
--- src/stock/CurrentInfoController.go
+++ src/stock/CurrentInfoController.go
@@ -9,6 +9,9 @@ import (
 	"stock/src/models"
 
 	"strings"
+
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
 )
 
 // FetchCurrentInfo 获取当前值
@@ -19,7 +22,12 @@ func (s *Stock) FetchCurrentInfo() {
 		panic(err)
 
 	}
-	body, err := ioutil.ReadAll(resp.Body)
+	utf8Reader := transform.NewReader(resp.Body,
+		simplifiedchinese.GBK.NewDecoder())
+	body, err := ioutil.ReadAll(utf8Reader)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer resp.Body.Close()
 	// 股票名称、今日开盘价、昨日收盘价、当前价格、今日最高价、今日最低价、竞买价、竞卖价、成交股数、成交金额、买1手、买1报价、买2手、买2报价、…、买5报价、…、卖5报价、日期、时间
 	strArr := strings.Split(string(body), ",")
@@ -27,8 +35,13 @@ func (s *Stock) FetchCurrentInfo() {
 	ci := &models.CurrentInfo{}
 
 	st := reflect.ValueOf(ci).Elem()
-	for k, v := range strArr[1 : len(strArr)-2] {
+	for k, v := range strArr[:len(strArr)-3] {
+		if k == 0 {
+			s.Name = strings.Split(v, "\"")[1]
+			continue
+		}
 		st.Field(k).SetString(v)
+
 	}
 
 	s.CurrentInfo = ci
