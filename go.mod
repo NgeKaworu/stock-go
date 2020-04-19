5c3af7c445cd9e6274307a86bf1acb2658725807
NgeKaworu <NgeKaworu@163.com>
2020-04-19 15:28:05 +0800

    加入gql

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module stock
 go 1.14
 
 require (
+	github.com/graph-gophers/graphql-go v0.0.0-20200309224638-dae41bde9ef9
 	go.mongodb.org/mongo-driver v1.3.1
 	golang.org/x/text v0.3.2
 )
--- main.go
+++ main.go
@@ -3,21 +3,33 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"path"
+	"path/filepath"
 	"stock/src/constants"
+	"stock/src/cors"
 	"stock/src/dbengin"
 	"stock/src/models"
 	"stock/src/stock"
 	"stock/src/utils"
+	"syscall"
 	"time"
+
+	"github.com/graph-gophers/graphql-go"
+	"github.com/graph-gophers/graphql-go/relay"
 )
 
 func main() {
 	var (
+		addr   = flag.String("l", ":8000", "绑定Host地址")
 		dbinit = flag.Bool("i", false, "init database flag")
 		mongo  = flag.String("m", "mongodb://localhost:27017", "mongod addr flag")
-		db     = flag.String("db", "stock", "database name")
+		db     = flag.String("db", "stock_test", "database name")
 		pb     = flag.Int("pb", 2, "pb weight")
 		pe     = flag.Int("pe", 5, "pe weight")
 		peg    = flag.Int("peg", 6, "peg weight")
@@ -31,8 +43,17 @@ func main() {
 	log.SetOutput(os.Stdout)
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := os.Stat(path.Join(dir, "/data")); err != nil {
+		log.Println(err)
+		return
+	}
+
 	eng := dbengin.NewDbEngine()
-	err := eng.Open(*mongo, *db, *dbinit)
+	err = eng.Open(*mongo, *db, *dbinit)
 
 	if err != nil {
 		log.Println(err.Error())
@@ -126,5 +147,52 @@ func main() {
 	} else {
 		log.Println(ret)
 	}
+
+	b, err := ioutil.ReadFile(filepath.Join(dir, "schema.graphql"))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	opts := []graphql.SchemaOpt{
+		graphql.UseFieldResolvers(),
+		graphql.UseStringDescriptions(),
+		graphql.MaxParallelism(1000),
+		//生产环境需启动禁调试功能
+		//	opts = append(opts, graphql.DisableIntrospection())
+	}
+
+	schema := graphql.MustParseSchema(string(b), eng, opts...)
+	mux := http.NewServeMux()
+	mux.Handle("/", &relay.Handler{Schema: schema})
+
+	srv := &http.Server{Handler: cors.CORS(mux), ErrorLog: nil}
+	srv.Addr = *addr
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	log.Println("server on http port", srv.Addr)
+
+	signalChan := make(chan os.Signal, 1)
+	cleanupDone := make(chan bool)
+	cleanup := make(chan bool)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		for range signalChan {
+			ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+			defer cancel()
+			go func() {
+				_ = srv.Shutdown(ctx)
+				cleanup <- true
+			}()
+			<-cleanup
+			eng.Close()
+			fmt.Println("safe exit")
+			cleanupDone <- true
+		}
+	}()
+	<-cleanupDone
 
 }
